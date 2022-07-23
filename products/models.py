2c49adb06e23c46ec523136aeae8620319b7198e
Hyeonjae-K <href.hjk@gmail.com>
2022-07-23 14:28:24 +0100

    ADD - Product 모델 추가

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    src = models.URLField(null=True, blank=True)
+    url = models.URLField()
+    update_date = models.DateField(auto_now=True)
+    create_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title}({self.author})'
