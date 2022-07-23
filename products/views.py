fa3b6edebcadf945f707529ed38563da1bfdd6d0
Hyeonjae-K <href.hjk@gmail.com>
2022-07-23 14:39:26 +0100

    FEAT - index 페이지 구현 및 등록

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from products.models import Product
+
+
+def index(request):
+    print('index')
+    products = Product.objects.order_by('-create_date')
+    return render(request, 'index.html', {'products': products})
