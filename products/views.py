97413fd56912105574e17c054e263f476fc72f1e
Hyeonjae-K <href.hjk@gmail.com>
2022-07-23 19:05:14 +0100

    FEAT - index 페이지 구현

--- products/views.py
+++ products/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
+from apscheduler.schedulers.background import BackgroundScheduler
+
 from products.models import Product
+from crawlers import products
+
+sched = BackgroundScheduler()
+sched.add_job(products.crawl_jgnara, 'interval', seconds=5)
+sched.start()
 
 
 def index(request):
-    print('index')
     products = Product.objects.order_by('-create_date')
     return render(request, 'index.html', {'products': products})
