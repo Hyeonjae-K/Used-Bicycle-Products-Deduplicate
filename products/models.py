a36762b7b74c29c42071a0674a211f31671cc001
Hyeonjae-K <href.hjk@gmail.com>
2022-07-30 16:25:08 +0900

    FIX - 모델 수정

--- products/models.py
+++ products/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
-    author = models.CharField(max_length=50)
+    author = models.CharField(max_length=50, null=True, blank=True)
+    price = models.IntegerField(null=True, blank=True)
+    location = models.CharField(max_length=50, null=True, blank=True)
     src = models.URLField(null=True, blank=True)
     url = models.URLField()
     update_date = models.DateTimeField(auto_now=True)
