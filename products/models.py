5f555e5f20a6a8a92d09b18a5bddbb6156b46ff1
Hyeonjae-K <href.hjk@gmail.com>
2022-08-14 16:53:55 +0900

    FIX - 필드 수정

--- products/models.py
+++ products/models.py
@@ -5,9 +5,9 @@ class Product(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=50, null=True, blank=True)
     price = models.IntegerField(null=True, blank=True)
-    location = models.CharField(max_length=50, null=True, blank=True)
     src = models.URLField(null=True, blank=True, max_length=1024)
     url = models.URLField(max_length=1024)
+    market = models.CharField(max_length=10, null=True, blank=True)
     update_date = models.DateTimeField(auto_now=True)
     create_date = models.DateTimeField(auto_now_add=True)
 
