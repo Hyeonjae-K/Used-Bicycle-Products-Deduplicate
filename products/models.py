829f1ab2bef88868e8b353349f1f237b1e6f385c
Hyeonjae-K <href.hjk@gmail.com>
2022-08-16 17:25:40 +0900

    FIX - 제목 길이 초과로 인한 오류 수정

--- products/models.py
+++ products/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=50, null=True, blank=True)
+    title = models.CharField(max_length=256)
+    author = models.CharField(max_length=128, null=True, blank=True)
     price = models.IntegerField(null=True, blank=True)
     src = models.URLField(null=True, blank=True, max_length=1024)
     url = models.URLField(max_length=1024)
-    market = models.CharField(max_length=10, null=True, blank=True)
+    market = models.CharField(max_length=8, null=True, blank=True)
     update_date = models.DateTimeField(auto_now=True)
     create_date = models.DateTimeField(auto_now_add=True)
 
