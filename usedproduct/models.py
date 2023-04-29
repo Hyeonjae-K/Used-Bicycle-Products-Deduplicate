cec6921f8ab37737b407d59b92207d2679cb663a
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-04-29 16:52:48 +0900

    feat: 데이터베이스 모델 구현

--- usedproduct/models.py
+++ usedproduct/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+STORE_CHOICES = (
+    ('중나', '중고나라'),
+    ('번장', '번개장터'),
+)
+
+
+class Category(models.Model):
+    store = models.CharField(max_length=2,  choices=STORE_CHOICES)
+    name = models.CharField(max_length=25)
+    url = models.URLField()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.IntegerField()
+    image = models.URLField()
+    author = models.CharField(max_length=15)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
